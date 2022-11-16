373a55980e9764e7c36c7ae00df572e3e59278a4
Arman Barraghi <abarraghi@gmail.com>
2022-11-16 13:36:06 +0800

    added calculate percentage logic to main class

--- file2.java
+++ file2.java
@@ -1,10 +1,20 @@
 public class file2 {
 
 
-    public static void main(String[] args){
+	public static void main(String[] args){
 
-        System.out.println("Hello world!");
+		System.out.println("Hello world!");
 
-    }
+		System.out.println(calculatePercentage(0.75));
+
+    	}
+
+	public static double calculatePercentage(double proportion) {
+
+		return proportion*100;
+
+	}
+
+
 
 }
